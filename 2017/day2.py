3cdfb97d72b25f1689526dde88512d70c9447ecd
Jonatan Magnusson <jonatan@myeden.se>
2017-12-15 09:48:30 +0100

    Add solution, day 2

---
+++ 2017/day2.py
@@ -0,0 +1,37 @@
+
+ex1 = """5 1 9 5
+7 5 3
+2 4 6 8"""
+
+ex2 = """5 9 2 8
+9 4 7 3
+3 8 6 5"""
+
+def part1(txt):
+    data = [[int(x) for x in line.split()] for line in txt.splitlines()]
+    return sum(max(line) - min(line) for line in data)
+
+def part2(txt):
+    data = [[int(x) for x in line.split()] for line in txt.splitlines()]
+
+    def chk(line):
+        s = 0
+        for i in range(len(line)):
+            a = line[i]
+            for j in range(len(line)):
+                b = line[j]
+                if a != b:
+                    if a % b == 0:
+                        s += a // b
+        return s
+
+    return sum(chk(line) for line in data)
+
+with open("day2.txt") as fp:
+    data = fp.read()
+
+print(part1(ex1))
+print(part1(data))
+print(part2(ex2))
+print(part2(data))
+
