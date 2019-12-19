11870742135a19aac80cc3b51a153dc843d43b65
Jonatan Magnusson <jonatan.magnusson@above.se>
2019-12-20 00:03:39 +0100

    Add general purpose canvas (2d graphics buffer)

---
+++ 2019/day15/Canvas.go
@@ -0,0 +1,97 @@
+package main
+
+import "fmt"
+
+type Coordinate struct {
+	x, y int
+}
+
+type Sprite struct {
+	x, y int
+	char byte
+}
+
+// Canvas - display buffer
+type Canvas struct {
+	width   int
+	height  int
+	buffer  []byte
+	border  bool
+	columns int
+	rows    int
+	offset  Coordinate
+}
+
+// NewCanvas - Allocate, initiate and return a new Canvas
+func NewCanvas(width, height int, border bool, fill byte) (canvas *Canvas) {
+	canvas = new(Canvas)
+	canvas.width = width
+	canvas.height = height
+	canvas.columns = width + 1
+	canvas.rows = height
+	canvas.offset = Coordinate{0, 0}
+
+	if border {
+		canvas.columns += 2
+		canvas.rows += 2
+		canvas.offset = Coordinate{1, 1}
+	}
+
+	canvas.buffer = make([]byte, (canvas.columns)*canvas.rows)
+
+	for i := range canvas.buffer {
+		if (i+1)%(canvas.columns) == 0 {
+			canvas.buffer[i] = '\n'
+		} else {
+			canvas.buffer[i] = fill
+		}
+	}
+
+	if border {
+		for i := 1; i < canvas.columns-2; i++ {
+			canvas.buffer[i] = '-'
+			canvas.buffer[canvas.columns*(canvas.rows-1)+i] = '-'
+		}
+
+		for i := 1; i < canvas.rows-1; i++ {
+			canvas.buffer[canvas.columns*i] = '|'
+			canvas.buffer[canvas.columns*(i+1)-2] = '|'
+		}
+
+		canvas.buffer[0] = '+'
+		canvas.buffer[canvas.columns-2] = '+'
+		canvas.buffer[canvas.columns*(canvas.rows-1)] = '+'
+		canvas.buffer[canvas.columns*canvas.rows-2] = '+'
+	}
+
+	return canvas
+}
+
+// Get - get character at x,y
+func (c *Canvas) Get(x, y int) byte {
+	idx := (y+c.offset.y)*c.columns + x + c.offset.x
+	return c.buffer[idx]
+}
+
+// Set - set character at x,y
+func (c *Canvas) Set(x, y int, char byte) {
+	idx := (y+c.offset.y)*c.columns + x + c.offset.x
+	c.buffer[idx] = char
+}
+
+// Render - render display buffer
+func (c *Canvas) Render(sprites []Sprite) {
+	prev := make([]byte, len(sprites))
+
+	for i, sprite := range sprites {
+		prev[i] = c.Get(sprite.x, sprite.y)
+		c.Set(sprite.x, sprite.y, sprite.char)
+	}
+
+	fmt.Println("\033[0;0H")
+	fmt.Print(string(c.buffer))
+
+	for i, sprite := range sprites {
+		c.Set(sprite.x, sprite.y, prev[i])
+	}
+}
