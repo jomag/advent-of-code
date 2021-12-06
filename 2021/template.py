0e67d00aa0b78d7255b4d0c209b3fa10eb623f4b
Jonatan Magnusson <jonatan.magnusson@above.se>
2021-12-06 08:53:13 +0100

    Add verbose mode and default to only run with test data

--- 2021/template.py
+++ 2021/template.py
@@ -1,8 +1,8 @@
-def part1(data):
+def part1(data, verbose=False):
     return 1
 
 
-def part2(data):
+def part2(data, verbose=False):
     return 2
 
 
@@ -12,7 +12,7 @@ with open("input.txt") as f:
 with open("example.txt") as f:
     example = [ln.strip() for ln in f.readlines()]
 
-print(f"Part 1 with example data: {part1(example)}")
-print(f"Part 1 with real input: {part1(lines)}")
-print(f"Part 2 with example data: {part2(example)}")
-print(f"Part 2 with real input: {part2(lines)}")
+print(f"Part 1 with example data: {part1(example, verbose=True)}")
+# print(f"Part 1 with real input: {part1(lines)}")
+# print(f"Part 2 with example data: {part2(example, verbose=True)}")
+# print(f"Part 2 with real input: {part2(lines)}")
